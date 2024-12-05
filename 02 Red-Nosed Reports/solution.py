fbcf49641238c7ee9390c3497d99d139aa10d327
Daniel Comaduran <dcomadur@ucalgary.ca>
2024-12-05 10:14:37 -0700

    Refactor day 2, part 2

--- 02 Red-Nosed Reports/solution.py
+++ 02 Red-Nosed Reports/solution.py
@@ -1,66 +1,47 @@
 #%% Load data
-# Read code file and store as list of values
 with open("input02.txt") as file:
     data = file.read().splitlines()
 
+#%% Functions
+def check_step(a, b):
+    if a < b:
+        return 1 if b - a <= 3 else 0
+    elif a > b:
+        return -1 if a - b <= 3 else 0
+    return 0
+
+def is_safe(row_of_ints):
+    for n in range(len(row_of_ints) - 1):
+        step = check_step(row_of_ints[n], row_of_ints[n + 1])
+        if step == 0 or (n > 0 and step != check_step(row_of_ints[n - 1], row_of_ints[n])):
+            return False
+    return True
+    
 #%% Part 01
-safe_reports = [None] * len(data)
+safe_reports = 0
 
-def check_step(current_number, next_number):
-    if (current_number > next_number) and (current_number <= next_number + 3):
-        return 1
-    elif (current_number < next_number) and (current_number >= next_number - 3):
-        return -1
-    else:
-        return 0
+for row in data:
+    row_of_ints = list(map(int, row.split()))
     
-# Check each row
-for row in range(len(data)):
-    row_of_ints = list(map(int, data[row].split()))
-    safe_reports[row] = 1
-    
-    # Check each list of numbers per row
-    for (n,number) in enumerate(row_of_ints):
-        
-        if (n == 0):
-            previous_step = check_step(number, row_of_ints[n+1])
+    if is_safe(row_of_ints):
+        safe_reports += 1
 
-        elif (n < len(row_of_ints)-1):
-            current_step = check_step(number, row_of_ints[n+1])
-
-            if (current_step == 0) or (previous_step == 0) or (previous_step != current_step):
-                safe_reports[row] = 0
-                break
-        
-total_safe_reports = sum(safe_reports)
-print(f'Total safe reports: {total_safe_reports}')
+print(f'Total safe reports: {safe_reports}')
 
 #%% Part 02
-individual_steps = [None] * len(data)
+safe_reports = 0
 
 # Check each row
-for row in range(len(data)):
-    row_of_ints = list(map(int, data[row].split()))
-    safe_reports[row] = 1
-    row_error_flag = False
+for row in data:
+    row_of_ints = list(map(int, row.split()))
     
-    # Check each list of numbers per row
-    for (n,number) in enumerate(row_of_ints):
-        
-        if (n == 0):
-            previous_step = check_step(number, row_of_ints[n+1])
+    if is_safe(row_of_ints):
+        safe_reports += 1
+    else:
+        for n in range(len(row_of_ints)):
+            row_without_bad_number = row_of_ints[:n] + row_of_ints[n+1:]
+            if is_safe(row_without_bad_number):
+                safe_reports += 1
+                break
 
-        elif (n < len(row_of_ints)-1):
-            current_step = check_step(number, row_of_ints[n+1])
-
-            if (current_step == 0) or (previous_step == 0) or (previous_step != current_step):
-                if (row_error_flag == False): 
-                    row_error_flag = True
-                else:
-                    safe_reports[row] = 0
-                    break
-        
-total_safe_reports = sum(safe_reports)
-print(f'Total safe reports: {total_safe_reports}')
-
-# %%
+print(f'Total safe reports: {safe_reports}')
